d78fec42cbe768045b630d768597864936e5a68c
vdeturckheim <vlad2t@hotmail.com>
2019-09-13 09:25:36 +0200

    formatting

--- src/components/post/index.vue
+++ src/components/post/index.vue
@@ -21,9 +21,9 @@ export default {
     price: function() {
       return this.post.price || this.post.PRICE;
     },
-      priceUSD: function() {
-          return this.post.priceUSD || this.post.PRICE_USD;
-      },
+    priceUSD: function() {
+      return this.post.priceUSD || this.post.PRICE_USD;
+    },
     postTitle: function() {
       return this.post.title || this.post.TITLE;
     },
